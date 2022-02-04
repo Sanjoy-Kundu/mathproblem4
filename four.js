a6474c48fab22b3cc5c402bd39c19028ba88b79e
sanjoy <sanjoykundu187@gamil.com>
2022-02-04 00:24:29 -0500

    first commit

---
+++ four.js
@@ -0,0 +1,27 @@
+/*===========================================================================
+তুমি কি দুইটা ভেরিয়েবল এরমধ্যে তুলনা করতে পারো। কম্পারিজন করতে পারো। যে দুইটা ভেরিয়েবল এর মধ্যে প্রথমটা সেকেন্ডটা এর চাইতে ছোট, বড়, অসমান, সমান , ছোট বা সমান, বড় বা সমান। এইগুলা চেক করতে পারো। অর্থাৎ <, >, ==, !=, <=, >= চিহ্নগুলা ব্যবহার করতে পারো। তাহলে দুইটা সংখ্যা টাইপের ভেরিয়েবল ডিক্লেয়ার করে তাদেরকে এই ছয়ভাবে তুলনা করে কোড লিখে ফেলো। 
+===========================================================================*/
+
+var first = 5;
+var second = 4;
+if (first > second) {
+	console.log("smaim is pass");
+}
+else if (first == second) {
+	console.log("You are fail");
+}
+else if (first != second) {
+	console.log("You are bravo!");
+}
+else if (first < second) {
+	console.log("Kamrul is reading");
+}
+else if (fist >= second) {
+	console.log("Bangladesh is good");
+}
+else if (first <= second) {
+	console.log("India is  best");
+}
+else {
+	console.log("we have own the game");
+}
